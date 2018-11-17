4c2e353f29fe6082463944db5c6b5fd657dd943e
NonlinearFruit <1123benji5813@gmail.com>
2018-11-17 13:57:35 -0600

    minor change

--- src/retrospector/core/boundry/Presenter.java
+++ src/retrospector/core/boundry/Presenter.java
@@ -8,7 +8,10 @@ import retrospector.core.request.model.RequestableReview;
 
 public interface Presenter {
     public void mediaAdded(RequestableMedia media);
+    public void mediaRetrieved(RequestableMedia convert);
+    public void mediaUpdated(RequestableMedia convert);
     public void mediaDeleted(int mediaId);
+    public void mediaRetrievedAll(List<RequestableMedia> media);
     
     public void factoidAdded(RequestableFactoid factoid);
     public void factoidRetrieved(RequestableFactoid factoid);
@@ -16,11 +19,7 @@ public interface Presenter {
     public void factoidDeleted(int factoidId);
 
     public void reviewAdded(RequestableReview requestableReview);
-    public void reviewUpdated(RequestableReview requestableReview);
     public void reviewRetrieved(RequestableReview requestableReview);
+    public void reviewUpdated(RequestableReview requestableReview);
     public void reviewDeleted(Integer id);
-
-    public void mediaRetrieved(RequestableMedia convert);
-    public void mediaUpdated(RequestableMedia convert);
-    public void mediaRetrievedAll(List<RequestableMedia> media);
 }
